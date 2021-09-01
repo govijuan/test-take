d6ff20289fd0eb463f0719456377b40b08afbcaa
Juan Carlos Coral <govindasd@gmail.com>
2021-09-01 10:38:47 -0300

    Styled form for the SortForm created

---
+++ src/components/molecules/SortForm/SortForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import StyledForm from './SortForm.styles'
+
+const SortForm = () => (
+    <StyledForm>
+        This is the styled form
+    </StyledForm>
+)
+
+export default SortForm
